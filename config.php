47f533c354aa68038512fdcd83e6dfdb006f55b2
Roonie Hsieh <wei225890@hotmail.com>
2019-09-11 23:15:59 +0800

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
 	require "vendor/autoload.php";
 
 	$FB = new Facebook\Facebook([
-	    'app_id' => '1327405310767128',
-	    'app_secret' => '2e40ddc68857112c4f64866e8489031c',
+	    'app_id' => '',
+	    'app_secret' => '',
 	    'default_graph_version' => 'v4.0'
 	]);
 
